f70e52e1d5fe41a397414d4b9e2bf5db02cd940e
drjanastasi <92044366+drjanastasi@users.noreply.github.com>
2025-11-24 00:55:58 +0100

    Update LB_1_r-1.cpp

--- LB_1_r-1/LB_1_r-1/LB_1_r-1.cpp
+++ LB_1_r-1/LB_1_r-1/LB_1_r-1.cpp
@@ -4,8 +4,9 @@ int main(array<System::String^>^ args)
 {
     String^ first(L"Ïðèâiò. Öå ìié ïåðøèé ïðîºêò.");
     String^ second(L" ÿ ìàëî ÷îãî ðîçóìiþ....Àëå(!!!) ìåíi äóæå öoêàâî:)");
-    String^ third(first + L" I " + second);
+    String^ third(first + L"I" + second);
     Console::WriteLine(third);
     return 0;
 }
+
 
